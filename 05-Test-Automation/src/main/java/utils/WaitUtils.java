1de39ae7a938ba1fc8bd1dc5a26b4bec9186ad47
Selvan L <116616124+selvan-75-it@users.noreply.github.com>
2026-08-21 23:11:57 +0530

    Test-Scenarios.md
    Implemented WaitUtils class for Selenium WebDriver to handle element visibility and clickability.

---
+++ 05-Test-Automation/src/main/java/utils/WaitUtils.java
@@ -0,0 +1,33 @@
+package utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitUtils {
+
+    private final WebDriverWait wait;
+
+    public WaitUtils(WebDriver driver) {
+        this.wait = new WebDriverWait(
+                driver,
+                Duration.ofSeconds(10)
+        );
+    }
+
+    public WebElement waitForVisibility(By locator) {
+        return wait.until(
+                ExpectedConditions.visibilityOfElementLocated(locator)
+        );
+    }
+
+    public WebElement waitForClickable(By locator) {
+        return wait.until(
+                ExpectedConditions.elementToBeClickable(locator)
+        );
+    }
+}
